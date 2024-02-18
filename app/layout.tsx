d431f0be02e83d820711d611d82b3342248596ce
RushabhBhosale <145191870+RushabhBhosale@users.noreply.github.com>
2024-02-18 18:44:22 +0530

    updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { Nunito } from "next/font/google";
 import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
+import Modal from "./components/modals/Modal";
 
 export const metadata: Metadata = {
   title: "Airbnb",
@@ -20,6 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <Modal actionLabel="submit" title="Hello" isOpen />
         <Navbar />
         {children}
       </body>
